59cb1d2143b4155d33a642138c22782a92a5aa6e
Gen1usG <812487021@qq.com>
2020-06-29 18:46:16 +0800

    deploy

---
+++ precache-manifest.4ab67bf07843db7d94d7c2c3190cd92b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7556ecb98f6375f57efb5cd513795c37",
+    "url": "/banana-ledger-website/index.html"
+  },
+  {
+    "revision": "09715dceb0eaea3ffcb9",
+    "url": "/banana-ledger-website/static/css/2.0fb3545e.chunk.css"
+  },
+  {
+    "revision": "665166f4875d37286911",
+    "url": "/banana-ledger-website/static/css/main.78cf82c8.chunk.css"
+  },
+  {
+    "revision": "09715dceb0eaea3ffcb9",
+    "url": "/banana-ledger-website/static/js/2.b3be4281.chunk.js"
+  },
+  {
+    "revision": "99406820fc9e02f862dbd66e126ca114",
+    "url": "/banana-ledger-website/static/js/2.b3be4281.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "665166f4875d37286911",
+    "url": "/banana-ledger-website/static/js/main.3097db7d.chunk.js"
+  },
+  {
+    "revision": "831da2132fb6f57f3a56",
+    "url": "/banana-ledger-website/static/js/runtime-main.23384a82.js"
+  }
+]);
